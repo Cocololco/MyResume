c109184d4a7a7b81023d887a51c2198d6d397204
Cocololco <116242938+Cocololco@users.noreply.github.com>
2024-09-10 16:29:46 +1000

    Update resume + mini game

---
+++ game.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Resume Corentin Hillion">
+    <meta name="author" content="Corentin Hillion">
+    <meta name="generator" content="Coco 1.0.0">
+    <title>Corentin Hillion - Resume</title>
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <?php include("function.php")?>
+  </head>
+  <body>
+    <!-- Navbar -->
+    <header data-bs-theme="dark">
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">Corentin Hillion</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="/index.php">Back</a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+      
+    <!-- Main Body -->
+    <main style="padding-top: 70px;"> 
+      <link rel="stylesheet" href="css/canon.css">
+      <div class="game-container">
+        <div class="playground" id="playground">
+        </div>
+        
+        <button id="resetButton" class="btn btn-secondary mt-1">Reset</button>
+        <div id="infoBox">  </div>
+        <script src="js/canon.js"></script>
+      </div>
+      
+    </main>
+    
+        
+  </body>
+</html>
--- index.php
+++ index.php
@@ -22,7 +22,7 @@
           <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">About Me</a>
+                <a class="nav-link" aria-current="page" href="#">About Me</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Experiences</a>
@@ -33,17 +33,17 @@
               <li class="nav-item">
                 <a class="nav-link" href="#">Contact</a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/game.php">Mini-Game</a>
+              </li>
             </ul>
-            <!-- <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> -->
           </div>
         </div>
       </nav>
     </header>
+
     <!-- Main Body -->
-    <main style="padding-top: 70px;">
+    <main style="padding-top: 70px;"> 
       <div id="carouselExampleControls" class="carousel slide " data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active category" data-bs-interval="1000000">
